c9e17bceb7c180237fbd8753fabf7aecf1453252
seitamuro <seitamuro0210@gmail.com>
2024-07-28 22:44:48 +0900

    Initial commit

---
+++ test/cdk-api-canary.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkApiCanary from '../lib/cdk-api-canary-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-api-canary-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkApiCanary.CdkApiCanaryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
