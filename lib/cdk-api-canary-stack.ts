2e2412adf31163691320303faf22f13e911c4114
seitamuro <seitamuro0210@gmail.com>
2024-08-05 23:19:44 +0900

    canary releaseを実装

--- lib/cdk-api-canary-stack.ts
+++ lib/cdk-api-canary-stack.ts
@@ -1,16 +1,54 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkApiCanaryStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const helloFunction = new NodejsFunction(this, "HelloFunction", {
+      entry: "lambda/hello.ts",
+      handler: "handler",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkApiCanaryQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloV2Function = new NodejsFunction(this, "HelloV2Function", {
+      entry: "lambda/hellov2.ts",
+      handler: "handler",
+    });
+
+    const api = new apigw.RestApi(this, "HelloApi");
+
+    const helloIntegration = new apigw.LambdaIntegration(helloFunction);
+    api.root.addMethod("GET", helloIntegration);
+
+    const deployment = new apigw.CfnDeployment(this, "MyDeployment", {
+      restApiId: api.restApiId,
+      stageName: "prod",
+      /*deploymentCanarySettings: {
+        percentTraffic: 10,
+        stageVariableOverrides: {
+          lambdaAlias: "canary",
+        },
+      },*/
+    });
+
+    new apigw.CfnStage(this, "MyStage", {
+      restApiId: api.restApiId,
+      stageName: "prod",
+      deploymentId: deployment.ref,
+      /*canarySetting: {
+        percentTraffic: 10,
+        stageVariableOverrides: {
+          lambdaAlias: "canary",
+        },
+        useStageCache: false,
+      },*/
+    });
+
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url || "Something went wrong with the deploy",
+    });
   }
 }
